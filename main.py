2dfc3a08a497ac5dec36ea9328226fe59b2e4bf7
Judika Pasaribu <156697722+judikapasaribu@users.noreply.github.com>
2024-01-16 21:07:48 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+#The following codes can only run in Kodland platform.
+#If you want to run it in your local computer, some settings must be adjusted.
 
+#pgzero
+
+WIDTH = 600 # Lebar window
+HEIGHT = 300 # Tinggi window
+
+TITLE = "Judul Game" # Judul untuk window game
+FPS = 30 # Jumlah frame per detik
